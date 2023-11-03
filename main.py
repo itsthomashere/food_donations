d69012e4265d70427af77463a240156242852c6d
itsthomashere <thomasadam.leigh@gmail.com>
2023-11-03 18:43:21 +0200

    Updating option menu

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+st.set_page_config(
+    page_title="Woolworths Project",
+    page_icon="🤖",
+    layout="wide"
+)
 
 title = "Woolworths Project"
 title = st.markdown(
@@ -10,9 +15,25 @@ title = st.markdown(
 menu = option_menu(
     menu_title=None,
     options=["Donations", "Barcode Scanner", "Dataset"],
-    icons=["suit-heart-fill", "piggy-bank", "piggy-bank"],  # https://icons.getbootstrap.com/
+    icons=["clipboard-data", "upc-scan", "database"],  # https://icons.getbootstrap.com/
     menu_icon="menu-up",
-    default_index=0,
-    orientation="vertical",
+    default_index=1,
+    orientation="horizontal",
     styles=None,
 )
+
+def access_donations():
+    st.title("Donations Received Today:")
+
+def receive_barcodes():
+    st.text_input("Enter barcode:")
+
+def view_dataset():
+    st.title("Woolworths Dataset:")
+pages = {
+    "Donations": access_donations,
+    "Barcode Scanner": receive_barcodes,
+    "Dataset": view_dataset
+}
+
+pages[menu]()
