270033ea885763d7fa97df7c2a6247ae337b12e9
itsthomashere <thomasadam.leigh@gmail.com>
2023-11-03 19:45:48 +0200

    Updating option menu

--- main.py
+++ main.py
@@ -1,39 +1,57 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-st.set_page_config(
-    page_title="Woolworths Project",
-    page_icon="🤖",
-    layout="wide"
-)
 
-title = "Woolworths Project"
-title = st.markdown(
-    f"<h1 style='text-align: center;'>{title}</h1>", unsafe_allow_html=True
-)
+def page_setup(page_title: str, layout: str) -> None:
+    st.set_page_config(page_title=page_title, page_icon="🤖", layout=layout)
 
-menu = option_menu(
-    menu_title=None,
-    options=["Donations", "Barcode Scanner", "Dataset"],
-    icons=["clipboard-data", "upc-scan", "database"],  # https://icons.getbootstrap.com/
-    menu_icon="menu-up",
-    default_index=1,
-    orientation="horizontal",
-    styles=None,
-)
+
+def display_title(title: str) -> None:
+    st.markdown(
+        body=f"<h1 style='text-align: center;'>{title}</h1>", unsafe_allow_html=True
+    )
+
+
+def main():
+    page_title = "Woolworths Project"
+    page_setup(page_title=page_title, layout="wide")
+    display_title(page_title)
+
+    menu = option_menu(
+        menu_title=None,
+        options=["Donations", "Barcode Scanner", "Dataset"],
+        icons=[
+            "clipboard-data",
+            "upc-scan",
+            "database",
+        ],  # https://icons.getbootstrap.com/
+        #menu_icon="menu-up",
+        default_index=1,
+        orientation="horizontal",
+        styles=None,
+    )
+
+    pages = {
+        "Donations": access_donations,
+        "Barcode Scanner": receive_barcodes,
+        "Dataset": view_dataset,
+    }
+    st.write(menu)
+
+    pages[menu]()
+
 
 def access_donations():
     st.title("Donations Received Today:")
 
+
 def receive_barcodes():
     st.text_input("Enter barcode:")
 
+
 def view_dataset():
     st.title("Woolworths Dataset:")
-pages = {
-    "Donations": access_donations,
-    "Barcode Scanner": receive_barcodes,
-    "Dataset": view_dataset
-}
 
-pages[menu]()
+
+if __name__ == "__main__":
+    main()
